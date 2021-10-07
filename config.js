922f49028fdd33f660b5958a5bdb0a00484779c7
baothoa <92080174+baothoa@users.noreply.github.com>
2021-10-07 16:00:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
-const CONFIG = {
-    titleWeb : "Được r nè" ,
-    introTitle : 'Bảo, bịp' ,
-    introDesc : Thường  lệ  vốn  trái  đất
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+onst  CONFIG  =  {
+    titleWeb : "được rồi nè" ,
+    introTitle : 'Bảo bịp' ,
+    introDesc : thấy giỏi chưaa` ,
+    btnIntro : 'Ghơ á' ,
+    title : ' em quá hiền đúng khumm🥺 ' ,
+    desc :'Dám nói không🤨' ,
+    btn Có : 'em hiền nhấtt' ,
+    btnNo : 'Không' ,
+    question : 'Ngứa tay quá, quánh nhau hongg' ,
+    btnReply : 'Gửi cho Thoa ' ,
+    reply : 'hong anh sợ em lắmm' ,
+    mess : 'em biết mà 🥰. Yêu nhiều 😘😘 ' ,
+    messDesc : 'ship cho ly trà đào nhaa🥺. " ,
+    btn Chấp nhận : 'Okiiiii lun <3' ,
+    messLink : 'https://fb.com
 }
